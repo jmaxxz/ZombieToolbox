9eec18ef0f41bbde51f2d24f7fd79177651a31b5
Jmaxxz <jmaxxz@jmaxxz.com>
2012-03-24 19:22:34 -0500

    Implement naive file system handler (repeated code)

---
+++ DotNet/CoreExtensions/CoreExtensions/IO/FileSystemHelper.cs
@@ -0,0 +1,65 @@
+using System;
+using System.IO;
+using System.Linq;
+
+namespace ZombieToolbox.System
+{
+    public class FileSystemHelper
+    {
+        private readonly Action<FileInfo> _fileHandler;
+        private readonly Action<DirectoryInfo> _directoryHandler;
+        private readonly Action<Exception> _errorHandler;
+        public FileSystemHelper (Action<FileInfo> fileHandler, Action<DirectoryInfo> directoryHandler, Action<Exception> errorHandler)
+        {
+            _fileHandler = fileHandler;
+            _directoryHandler = directoryHandler;
+            _errorHandler =errorHandler;
+        }
+
+
+        public void HandlePath(string path)
+        {
+            try
+            {
+                if(File.Exists(path))
+                {
+                    _fileHandler(new FileInfo(path));
+                }
+                else if(Directory.Exists(path))
+                {
+                    _directoryHandler(new DirectoryInfo(path));
+                }
+            }
+            catch(Exception e)
+            {
+                _errorHandler(e);
+            }
+        }
+
+        public void HandlePathRecursively(string path)
+        {
+            try
+            {
+                if(File.Exists(path))
+                {
+                    _fileHandler(new FileInfo(path));
+                }
+                else if(Directory.Exists(path))
+                {
+                    var d =new DirectoryInfo(path);
+                    _directoryHandler(d);
+                    foreach(var f in d.GetFileSystemInfos())
+                    {
+                        HandlePath(f.FullName);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                _errorHandler(e);
+            }
+        }
+
+    }
+}
+
